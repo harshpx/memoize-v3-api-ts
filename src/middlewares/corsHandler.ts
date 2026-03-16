9f5e06775979f3fc09044ebf2b0ea22967028355
harshpx <harsh.rzf@gmail.com>
2026-03-16 15:59:02 +0530

    modified cors ocnfig

--- src/middlewares/corsHandler.ts
+++ src/middlewares/corsHandler.ts
@@ -1,7 +1,7 @@
 import { cors } from "hono/cors";
 
 export const corsHandler = cors({
-  origin: ["http://localhost:5173", "https://www.memoize.in"],
+  origin: ["http://localhost:5173", "https://www.memoize.in", "https://app.memoize.in"],
   allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowHeaders: ["Content-Type", "Authorization"],
   credentials: true,
